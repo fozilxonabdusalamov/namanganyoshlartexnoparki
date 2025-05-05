6f57503c642489169e037b229bbc08a43566d41b
FozilxonAbdusalamov <fozilxonabdusalamov2006@gmail.com>
2025-05-05 17:12:47 +0500

    last commit

--- src/news/News.jsx
+++ src/news/News.jsx
@@ -9,26 +9,26 @@ const data = [
   {
     id: 1,
     image: oneStudy,
-    infoName: `“Technovation Girls” tanlovi App Inventor mashg'ulotlari`,
-    info: `Andijon yoshlar texnoparkida Technovation girls tanlovi ishtirokchilarining navbatdagi App Inventor mashg'uloti va raqamli loyiha taqdimotlari bo'lib o'tdi.`,
+    infoName: `“Namangan yoshlar texnoparki tomonidan "InnoX" biznes-inkubator va akselerator dasturiga targ'ibot ishlari olib borilmoqda`,
+    info: `2025-yil 5-may kuni "Namangan Yoshlar Texnoparki" direktori R.Inamov boshchiligida mas'ul xodimlar tomonidan Namangan davlat universiteti talabalari uchun "Namangan Yoshlar Texnoparki" faoliyati toʻgʻrisida taqdimot oʻtkazildi.`,
   },
   {
     id: 2,
     image: twoStudy,
-    infoName: `“Technovation Boys” tanlovi App Inventor mashg'ulotlari`,
-    info: `Andijon yoshlar texnoparkida Technovation boys tanlovi ishtirokchilarining navbatdagi App Inventor mashg'uloti va raqamli loyiha taqdimotlari bo'lib o'tdi.`,
+    infoName: `“Namangan yoshlar texnoparki tomonidan "InnoX" biznes-inkubator va akselerator dasturiga targ'ibot ishlari olib borilmoqda`,
+    info: `Tadbir davomida ishtirokchilarga mazkur maskanda yoshlar uchun yaratilayotgan imkoniyatlar, zamonaviy infratuzilmalar hamda "InnoX" dasturi doirasida tashkil etiladigan treninglar va amaliyot jarayonlari bo'yicha toʻliq maʼlumotlar berildi`,
   },
   {
     id: 2,
     image: threeStudy,
-    infoName: `“Technovation Boys” tanlovi App Inventor mashg'ulotlari`,
-    info: `Andijon yoshlar texnoparkida Technovation boys tanlovi ishtirokchilarining navbatdagi App Inventor mashg'uloti va raqamli loyiha taqdimotlari bo'lib o'tdi.`,
+    infoName: `“Namangan yoshlar texnoparki tomonidan "InnoX" biznes-inkubator va akselerator dasturiga targ'ibot ishlari olib borilmoqda`,
+    info: `Ushbu jarayonda Namangan davlat universiteti mas'ullari hamda talabalari faol ishtirok etdilar va o'zlarini qiziqtirgan savollarga batafsil javob oldilar.`,
   },
   {
     id: 2,
     image: fourStudy,
-    infoName: `“Technovation Boys” tanlovi App Inventor mashg'ulotlari`,
-    info: `Andijon yoshlar texnoparkida Technovation boys tanlovi ishtirokchilarining navbatdagi App Inventor mashg'uloti va raqamli loyiha taqdimotlari bo'lib o'tdi.`,
+    infoName: `“Namangan yoshlar texnoparki tomonidan "InnoX" biznes-inkubator va akselerator dasturiga targ'ibot ishlari olib borilmoqda`,
+    info: `Targ'ibot ishlari davom etmoqda.`,
   },
 ];
 
