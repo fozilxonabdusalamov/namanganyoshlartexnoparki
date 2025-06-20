d62be5b1e61fc05cb9863080a959c1b58f6aa482
FozilxonAbdusalamov <fozilxonabdusalamov2006@gmail.com>
2025-06-20 22:14:24 +0500

    push image

--- src/news/data.js
+++ src/news/data.js
@@ -58,6 +58,54 @@ import tenImageFour from './image/21,05,2025/photo_2025-05-21_18-28-11.jpg'
 import tenImageFive from './image/21,05,2025/photo_2025-05-21_18-28-17.jpg'
 
 
+import elevenImageone from './image/22-may/photo_2025-05-21_15-09-49.jpg'
+import elevenImageTwo from './image/22-may/photo_2025-05-21_18-13-59.jpg'
+import elevenImagethree from './image/22-may/photo_2025-05-21_15-09-49 (2).jpg'
+import elevenImagefour from './image/22-may/photo_2025-05-21_15-09-49 (3).jpg'
+import elevenImagefive from './image/22-may/photo_2025-05-21_15-09-48.jpg'
+import elevenImageSix from './image/22-may/photo_2025-05-21_15-09-48 (6).jpg'
+import elevenImageseven from './image/22-may/photo_2025-05-21_15-09-48 (5).jpg'
+import elevenImageeight from './image/22-may/photo_2025-05-21_15-09-48 (4).jpg'
+import elevenImagenine from './image/22-may/photo_2025-05-21_15-09-48 (3).jpg'
+import elevenImageeleven from './image/22-may/photo_2025-05-21_15-09-48 (2).jpg'
+
+import onikkiImageOne from './image/21-may/four.jpg'
+import onikkiImageTwo from './image/21-may/one.jpg'
+import onikkiImageTree from './image/21-may/three.jpg'
+import onikkiImageFour from './image/21-may/two.jpg'
+
+
+import yOne from './image/28-may/one.jpg'
+import ytwo from './image/28-may/two.jpg'
+import ythree from './image/28-may/three.jpg'
+
+import uOne from './image/30-may/one.jpg'
+import uTwo from './image/30-may/two.jpg'
+import uThree from './image/30-may/three.jpg'
+
+import jOne from './image/2-iyun/one.jpg'
+import jTwo from './image/2-iyun/two.jpg'
+import jThree from './image/2-iyun/three.jpg'
+import jFour from './image/2-iyun/four.jpg'
+
+import tOne from './image/9-iyun/one.jpg'
+import tTwo from './image/9-iyun/two.jpg'
+import tThree from './image/9-iyun/three.jpg'
+
+import chOne from './image/13-iyun/one.jpg'
+import chTwo from './image/13-iyun/two.jpg'
+import chThree from './image/13-iyun/three.jpg'
+import chFour from './image/13-iyun/four.jpg'
+
+import olOne from './image/16-iyun/one.jpg'
+import olTwo from './image/16-iyun/two.jpg'
+import olThree from './image/16-iyun/three.jpg'
+import olFour from './image/16-iyun/four.jpg'
+
+import yigOne from './image/20-iyun/one.jpg'
+import yigTwo from './image/20-iyun/two.jpg'
+import yigThree from './image/20-iyun/three.jpg'
+import yigFour from './image/20-iyun/four.jpg'
 // import oneImageSix from
 // import oneImageOne from
 // import oneImageTwo from
@@ -159,12 +207,97 @@ Namangan Yoshlar Texnoparki yoshlarni qoʻllab-quvvatlaydi.
         id: 10,
         images: [tenImageTree, tenImageFour, tenImageFive],
         infoName: `"Namangan Yoshlar Texnoparkida “InnoX” dasturi doirasida “Brend nima va marketingning kuchi” mavzusida o‘quv seminari bo‘lib o‘tdi.
-Seminar malakali mutaxassislar tomonidan olib borilib, unda brendning biznesdagi o‘rni, bozor holatini shakllantirishdagi roli va mahsulot sifatini belgilashdagi ta’siri haqida batafsil ma’lumotlar berildi.`,
-        info: `Shuningdek, bugungi kunda yetakchi tashkilotlar tomonidan qo‘llanilayotgan marketing strategiyalari, ularning qanday shakllantirilgani va amalga oshirilish bosqichlari keng qamrovli tahlil qilindi. Ishtirokchilar zamonaviy marketing yondashuvlari hamda brendni bozorga to‘g‘ri olib chiqish usullari bilan tanishtirildi.
+`,
+        info: `Seminar malakali mutaxassislar tomonidan olib borilib, unda brendning biznesdagi o‘rni, bozor holatini shakllantirishdagi roli va mahsulot sifatini belgilashdagi ta’siri haqida batafsil ma’lumotlar berildi. Shuningdek, bugungi kunda yetakchi tashkilotlar tomonidan qo‘llanilayotgan marketing strategiyalari, ularning qanday shakllantirilgani va amalga oshirilish bosqichlari keng qamrovli tahlil qilindi. Ishtirokchilar zamonaviy marketing yondashuvlari hamda brendni bozorga to‘g‘ri olib chiqish usullari bilan tanishtirildi.
 “InnoX” biznes-inkubatori doirasida Namangan Yoshlar Texnoparkining mavjud imkoniyatlaridan to‘laqonli foydalangan holda o‘quv jarayonlari tizimli ravishda tashkil etilmoqda. Dastur yakunida dastlabki bosqchda ishtirok etgan qatnashchilarga maxsus sertifikatlar topshiriladi.
 Namangan Yoshlar Texnoparki yoshlarni har tomonlama qo‘llab-quvvatlashda davom etadi.
  `,
     },
+    {
+        id: 11,
+        images: [elevenImageSix, elevenImageTwo, elevenImageeight, elevenImageeleven, elevenImagefive, elevenImagefour, elevenImagenine, elevenImageone, elevenImageseven, elevenImagethree],
+        infoName: "Mirzo Ulug‘bek vorislari” tanlovining respublika bosqichiga rasman start berildi",
+        info: "21-may kuni Toshkent davlat iqtisodiyot universitetida “Mirzo Ulug‘bek vorislari” respublika tanlovining ochilish marosimi bo‘lib o‘tdi. Ushbu tanlov 2022-yildan buyon har yili an’anaviy tarzda tashkil etilib, iqtidorli yoshlarni aniqlash va qo‘llab-quvvatlashda muhim o‘rin tutadi. Ochilish marosimida Iste’dodli yoshlarni qo‘llab-quvvatlash “Ulug‘bek” jamg‘armasi, Oliy ta’lim, fan va innovatsiyalar vazirligi, Innovatsion rivojlanish agentligi, Yoshlar ishlari agentligi hamda Toshkent davlat iqtisodiyot universiteti rahbariyati va mas’ullari, shuningdek, tanlovda ishtirok etuvchi talabalar qatnashdi."
+
+    },
+    {
+        id: 12,
+        images: [onikkiImageOne, onikkiImageFour, onikkiImageTwo, onikkiImageTree],
+        infoName: "Namangan Yoshlar Texnoparkida “InnoX” dasturi doirasida “Startap loyihalarni texnik hujjatlarini shakllantirish va biznes reja tuzish” mavzusida o‘quv seminari bo‘lib o‘tdi.",
+        info: "Seminar malakali mutaxassislar tomonidan olib borilib, unda  “Startap loyihalarni texnik hujjatlarini shakllantirish va biznes reja tuzish” hamda “B2B va B2C sotuvlar” mavzulari keng yoritildi. Mashg‘ulot davomida o‘quvchilar startap loyihani hujjatlashtirishning ahamiyati, texnik hujjatlar tarkibi va biznes reja tuzish bosqichlari haqida nazariy va amaliy bilimlarga ega bo‘lishdi. Shuningdek, B2B va B2C savdo modellarining farqlari, ularning qaysi holatda qo‘llanishi va marketing yondashuvlaridagi tafovutlar haqida tushuncha hosil qilishdi. Natijada o‘quvchilar loyihani bozorga chiqarish, hujjatlashtirish va to‘g‘ri sotuv modelini tanlash bo‘yicha asosiy ko‘nikmalarni egalladilar. Namangan Yoshlar Texnoparki yoshlarni har tomonlama qo‘llab-quvvatlashda davom etadi."
+    },
+    {
+        id: 13,
+        images: [yOne, ythree, ytwo],
+        infoName: "Namangan Yoshlar Texnoparkida “InnoX” dasturi doirasida “Biznes boshlashdan avvalgi jarayonlar,4P, Customer Development va Lean Canvas metodologiyasi” mavzusida o‘quv seminari bo‘lib o‘tdi.",
+        info: `Seminar malakali mutaxassislar tomonidan olib borilib, unda  Bugungi darsda "Biznes boshlashdan avvalgi jarayonlar: 4P, Customer Development va Lean Canvas metodologiyasi" mavzusi o‘rganildi.
+Unda marketingning 4P modeli (Mahsulot, Narx, Joylashuv, Reklama), mijozni aniqlash va ularning ehtiyojlarini o‘rganish (CusDev), hamda biznes g‘oyani 1 sahifalik reja asosida tahlil qilish (Lean Canvas) haqida batafsil ma’lumot berildi.
+Tinglovchilar ushbu metodologiyalarni tushunib olishdi, real muammo va echim asosida mijoz ehtiyojini aniqlash, marketing strategiyasini tuzish va oddiy biznes reja ishlab chiqish ko‘nikmalariga ega bo‘lishdi. Namangan Yoshlar Texnoparki yoshlarni har tomonlama qo‘llab-quvvatlashda davom etadi.`
+    },
+    {
+        id: 14,
+        images: [uOne, uTwo, uThree],
+        infoName: `Namangan Yoshlar Texnoparkida “InnoX” dasturi doirasida "Biznes boshlashdan avvalgi jarayonlar: SWOT tahlili va NST (muammo, sabab, yechim) metodikasi"   mavzusida o‘quv seminari bo‘lib o‘tdi.`,
+        info: `Seminar malakali mutaxassislar tomonidan olib borilib, unda  Bugungi darsda  "Biznes boshlashdan avvalgi jarayonlar: SWOT tahlili va NST (muammo, sabab, yechim) metodikasi" mavzusi o‘rganildi.
+Dars davomida SWOT tahlili orqali biznes g‘oyaning kuchli va zaif tomonlari, imkoniyatlar va tahdidlar aniqlash usullari ko‘rib chiqildi. Shuningdek, NST modeli yordamida real muammolarni aniqlash, ularning ildiz sabablari va aniq yechimlarni ishlab chiqish bo‘yicha amaliy mashg‘ulotlar olib borildi.
+Tinglovchilar o‘z biznes g‘oyalari asosida SWOT tahlili tuzishni va muammoga tayanib aniq yechim taklif qilish ko‘nikmalarini shakllantirishdi. Namangan Yoshlar Texnoparki yoshlarni har tomonlama qo‘llab-quvvatlashda davom etadi.
+`
+    },
+    {
+        id: 15,
+        images: [jOne, jTwo, jThree, jFour],
+        infoName: `Namangan Yoshlar Texnoparkida “InnoX” dasturi doirasida "Biznes uchun samarali marketing strategiyasini ishlab chiqish: Hududiy, Korporativ va Riteyl brending"  mavzusida o‘quv seminari bo‘lib o‘tdi.
+`,
+        info: `Seminar malakali mutaxassislar tomonidan olib borilib, unda  Bugungi darsda   "Biznes uchun samarali marketing strategiyasini ishlab chiqish: Hududiy, Korporativ va Riteyl brending" mavzusi o‘rganildi.
+Dars davomida marketing strategiyasi bosqichlari, maqsadli auditoriyani aniqlash va brend pozitsiyasini shakllantirish usullari ko‘rib chiqildi. Hududiy brending orqali mahsulotni joyga bog‘lash, korporativ brendingda kompaniya qadriyatlari va obro‘sini yaratish, riteyl brendingda esa chakana savdo nuqtalarida mijozga ta’sir o‘tkazish bo‘yicha amaliy misollar tahlil qilindi.
+Tinglovchilar samarali marketing yondashuvlarini tushunib, o‘z loyihalarida turli brending strategiyalarini qo‘llash bo‘yicha ko‘nikmalarga ega bo‘lishdi. Namangan Yoshlar Texnoparki yoshlarni har tomonlama qo‘llab-quvvatlashda davom etadi.`
+    },
+    {
+        id: 16,
+        images: [tOne, tThree, tTwo],
+        infoName: `Namangan Yoshlar Texnoparkida “InnoX” dasturi doirasida "Ilmiy g'oya yo'nalishi bo'yicha normativ hujjatlarni shakllantirish"  mavzusida o‘quv seminari bo‘lib o‘tdi.`,
+        info: `Seminar malakali mutaxassislar tomonidan olib borilib, unda Bugungi darsda "Ilmiy g'oya yo'nalishi bo'yicha normativ hujjatlarni shakllantirish" mavzusi o‘rganildi.
+Dars davomida ilmiy g'oyani shakllantirish bosqichlari, harajatlar smetasini tuzish, kalendar rejani shakillantirish bosqichlari o'rganildi.
+Tinglovchilar samarali marketing yondashuvlarini tushunib, o‘z loyihalarida turli brending strategiyalarini qo‘llash bo‘yicha ko‘nikmalarga ega bo‘lishdi. Namangan Yoshlar Texnoparki yoshlarni har tomonlama qo‘llab-quvvatlashda davom etadi.
+`
+    },
+    {
+        id: 17,
+        images: [chOne, chTwo, chThree, chFour],
+        infoName: `Namangan Yoshlar Texnoparkida “InnoX” dasturi doirasida “Startap loyiha uchun muvaffaqiyatli jamoa va hamkor topish” mavzusida o‘quv seminari bo‘lib o‘tdi.`,
+        info: `Seminar malakali mutaxassislar tomonidan olib borilib, unda Bugun darsda “Startap loyiha uchun muvaffaqiyatli jamoa va hamkor topish” mavzusi o‘rganildi. Mashg‘ulot davomida o‘quvchilar startap muvaffaqiyatining muhim omillaridan biri bu – kuchli va malakali jamoa ekanligini anglab yetishdi. Loyihani muvaffaqiyatli yuritish uchun turli sohalardagi mutaxassislar — texnik, marketing, moliyaviy va boshqaruv yo‘nalishlarida bilimga ega bo‘lgan jamoa a’zolari kerakligi tushuntirildi. Shuningdek, strategik hamkorlar topish, ya’ni investorlar, ishlab chiqaruvchilar yoki bozor bilan bog‘lovchi tashkilotlar bilan aloqalar o‘rnatish yo‘llari muhokama qilindi. Namangan Yoshlar Texnoparki yoshlarni har tomonlama qo‘llab-quvvatlashda davom etadi.`
+    },
+    {
+        id: 18,
+        images: [olOne, olTwo, olThree, olFour],
+        infoName: `Namangan Yoshlar Texnoparkida “InnoX” dasturi doirasida “Startap loyihaning biznes rejasini shakllantirish: rezyume, ijrochilar, loyiha tavsifi va marketing tahlili” mavzusida o‘quv seminari bo‘lib o‘tdi.`,
+        info: `Seminar malakali mutaxassislar tomonidan olib borilib, unda Bugun dars davomida “Startap loyihaning biznes rejasini shakllantirish: rezyume, ijrochilar, loyiha tavsifi va marketing tahlili” mavzusi o‘rganildi. Mashg‘ulot davomida o‘quvchilar biznes-rejaning asosiy bo‘limlari bilan tanishishdi. Jumladan, loyiha rezyumesini tuzish — ya’ni qisqa, aniq va investorlar uchun qiziqarli bo‘lgan umumiy ma’lumotlarni bayon etish ko‘nikmalari shakllantirildi. Shuningdek, loyiha ijrochilari haqida to‘liq va ishonchli ma’lumot berish, ularning malakasi va vazifalarini ko‘rsatish muhimligi tushuntirildi. Loyihaning maqsadi, yechim berayotgan muammosi va mahsulot yoki xizmatning o‘ziga xosligi haqida batafsil tavsif yozish bo‘yicha mashqlar bajarildi. Namangan Yoshlar Texnoparki yoshlarni har tomonlama qo‘llab-quvvatlashda davom etadi.
+`
+    },
+    {
+        id: 19,
+        images: [yigOne, yigFour, yigThree, yigTwo],
+        infoName: `Namangan Yoshlar Texnoparkida “InnoX” dasturi doirasida "Startaplarga investorlarni, venchur fondlarni va biznes angellarini jalb etish" mavzusida dars otilda. Dars davomida quyidagilar ozlashtirildi`,
+        info: `Dars davomida quyidagi savollarga javob topdik:
+ Investorlar startapdan nima kutadi?
+ Venchur fondlar qanday ishlaydi va qanday startaplarga qiziqadi?
+ Biznes angellar kimlar va ular bilan qanday aloqa qilish kerak?
+ Pitch tayyorlashning samarali usullari
+ MVP (Minimum Viable Product) va uning investorlar uchun ahamiyati 
+Misollar bilan tushuntirib o‘tdik:
+ Indiegogo kabi crowdfunding platformalari
+ Sequoia Capital – dunyoning eng yirik venchur fondlaridan biri
+ Pitching session nima va unga qanday tayyorgarlik ko‘riladi?
+
+ Maqsadimiz – o‘quvchilarni startap g‘oyasini investor oldida ishonchli taqdim etishga tayyorlash!
+`
+    },
+    // {
+    //     id: 20,
+    //     images: [],
+    //     infoName: "",
+    //     info: ""
+    // },
 ];
 
 export default data
